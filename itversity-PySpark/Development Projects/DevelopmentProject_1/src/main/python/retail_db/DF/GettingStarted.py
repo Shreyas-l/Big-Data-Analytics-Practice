26e9747c73a9e5f7b23b979fff64cfe40893d607
Shreyas-l <shreyas.labh@gmail.com>
2020-05-12 19:38:22 +0530

    Hadoop Lynda.com

--- itversity-PySpark/Development Projects/DevelopmentProject_1/src/main/python/retail_db/DF/GettingStarted.py
+++ itversity-PySpark/Development Projects/DevelopmentProject_1/src/main/python/retail_db/DF/GettingStarted.py
@@ -15,7 +15,7 @@ orders.printSchema()
 orders.describe().show()
 orders.show()
 
-from pyspark.sql.types import IntegerType
+from pyspark.sql.types import IntegerType,FloatType
 
 ordersDF = orders.withColumn('order_id',orders.order_id.cast(IntegerType())).\
                   withColumn('order_customerID',orders.order_customerID.cast(IntegerType()))
@@ -23,3 +23,21 @@ ordersDF = orders.withColumn('order_id',orders.order_id.cast(IntegerType())).\
 ordersDF.printSchema()
 ordersDF.describe().show()
 ordersDF.show()
+
+
+order_items = spark.read.csv("/Users/shreyas_rl/Desktop/git/itversity-data/retail_db/order_items").\
+              toDF('order_item_id','order_item_order_id','order_item_product_id','order_item_qty',
+                   'order_item_subtotal','order_item_price')
+order_items.printSchema()
+order_items.describe().show()
+order_items.show(5)
+
+order_itemsDF = order_items.withColumn('order_item_id',order_items.order_item_id.cast(IntegerType())).\
+                            withColumn('order_item_order_id',order_items.order_item_order_id.cast(IntegerType())).\
+                            withColumn('order_item_product_id',order_items.order_item_product_id.cast(IntegerType())).\
+                            withColumn('order_item_qty',order_items.order_item_qty.cast(IntegerType())).\
+                            withColumn('order_item_subtotal',order_items.order_item_subtotal.cast(FloatType())).\
+                            withColumn('order_item_price',order_items.order_item_price.cast(FloatType()))
+
+order_itemsDF.printSchema()
+order_itemsDF.show(5)
